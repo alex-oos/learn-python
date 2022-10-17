37e272ddf4050abaccf1de1c34f910d9f64309ee
Alex <Alex@qq.com>
2022-10-17 20:16:14 +0800

    单继承和多继承
    Signed-off-by: Alex <Alex@qq.com>

--- samples/oop_basic/person.py
+++ samples/oop_basic/person.py
@@ -11,28 +11,72 @@
 #    @Time : 2020/9/2 17:30
 #    @FIle： person.py
 #    @Software: PyCharm
-import time
 
 
 class Person(object):
+    # 定义基本属性
+    name = None
+    age = None
+    sex = None
+    # 定义私有属性,私有属性在类外部无法访问
+    __weight = None
 
-    def __init__(self,name,age,sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
-
-    def print(self):
-        print(" % s % d %s " % (self.name,self.age,self.sex))
+    # 定义构造方法
+    def __init__(self, n, a, s, w):
+        self.name = n
+        self.age = a
+        self.sex = s
+        self.__weight = w
 
     def add(self):
-        a = self.age+1
+        a = self.age + 1
+        return self.age + 1
 
-        return  self.age+1
+    def __str__(self):
+        return 'name: ' + str(self.name) + ' age:' + str(self.age) + ' sex: ' + str(self.sex) + ' __weight: ' + str(
+            self.__weight) + ' '
 
 
-person = Person('李将',15,'男')
+# 单继承示范
+class Student(Person):
+    grade = None
 
-person.print()
-a = person.add()
-print(a)
+    def __init__(self, n, a, s, w, g):
+        super().__init__(n, a, s, w)
+        self.grade = g
 
+    def __str__(self):
+        return super().__str__() + 'grade: ' + str(self.grade)
+
+
+class Speaker(object):
+    topic = None
+    name = None
+
+    def __int__(self, n, t):
+        self.name = n
+        self.topic = t
+        print(self.__str__(self))
+
+    def __str__(self):
+        return 'name: ' + str(self.name) + ' topic:' + str(self.topic)
+
+
+# 多继承示范
+class Sample(Speaker, Student):
+
+    def __int__(self, n, a, w, g, t):
+        Student.__init__(self, n, a, w, g)
+        Speaker.__init__(self, n, t)
+
+
+if __name__ == '__main__':
+    person = Person('Alex', 20, '男', 100)
+    print(person.__str__())
+    a = person.add()
+    print('年龄是：', a)
+    student = Student('Alex', 20, '男', 120, 100)
+    print(student.__str__())
+
+    sample = Sample('Alex', 20, '男', 120, 'python')
+    print(sample.__str__())
