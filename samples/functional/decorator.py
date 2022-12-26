1db66ce9ebe1df716c5b665765a1a234704b702a
alex <li_703@qq.com>
2022-12-26 11:43:56 +0800

    装饰器
    Signed-off-by: alex <li_703@qq.com>

--- samples/functional/decorator.py
+++ samples/functional/decorator.py
@@ -2,15 +2,88 @@
 # -*- coding: utf-8 -*-
 
 """
-装饰器： 
-  本质：在函数调用前后自动打印日志，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）。
-   其实就是使用闭包的概念，在其一个函数内，包含另外一个函数
-   @wraps接受一个函数来进行装饰，并加入了复制函数名称、注释文档、参数列表等等的功能。这可以让我们在装饰器里面访问在装饰之前的函数的属性，不加的话，就会呗函数内的操作给破坏掉
+装饰器：
+
+
 """
+import time
+from functools import wraps
 
 
-# 一、无参的装饰器
+# 无参装饰器
+def outer(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print('执行前')
+        res = func(*args, **kwargs)
+        print('执行后')
+        return res
 
-def log(func):
+    return inner
+
+
+@outer
+def fun1():
+    print('我是fun1函数')
+    a = 'fun1'
+    return a
+
+
+tmp = fun1()
+print(tmp)
+
+
+# 有参装饰器
+
+def outerp(text):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(text)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@outerp('test')
+def aa():
+    print('aaa')
+
+
+c = aa()
+
+
+# 练习题
+
+def metric(fn):
+    @wraps(fn)
     def wrapper(*args, **kwargs):
-        pass
+        begin_time = time.time()
+        result = fn(*args, **kwargs)
+        print(f'{fn.__name__} executed in {round(time.time() - begin_time, 4)} ms')
+        return result
+
+    return wrapper
+
+
+# 测试
+@metric
+def fast(x, y):
+    time.sleep(0.0012)
+    return x + y;
+
+
+@metric
+def slow(x, y, z):
+    time.sleep(0.1234)
+    return x * y * z;
+
+
+f = fast(11, 22)
+s = slow(11, 22, 33)
+if f != 33:
+    print('测试失败!')
+elif s != 7986:
+    print('测试失败!')
