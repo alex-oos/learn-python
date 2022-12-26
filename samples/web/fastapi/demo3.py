1fd81811ea8ca744d4eefacd44664e6330fe1160
alex <li_703@qq.com>
2022-12-26 14:53:07 +0800

    第一章-fastapi-请求体
    Signed-off-by: alex <li_703@qq.com>

---
+++ samples/web/fastapi/demo3.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    @Author : tangkaize
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#    @Time : 2022/12/26 12:45
+#    @FIle： demo3.py
+#    @Software: PyCharm
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+# 请求体
+
+@app.post('/items/')
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
+
+
+if __name__ == '__main__':
+    # swaager地址：http://127.0.0.1:8000/docs
+    uvicorn.run('demo3:app', host='127.0.0.1', port=8000, reload=True)
