b79e12c151b1b3790f7e4f20e131b48b8a50e30c
alex <li_703@qq.com>
2022-12-28 17:03:18 +0800

    第一章-fastapi-请求表单和文件
    Signed-off-by: alex <li_703@qq.com>

---
+++ samples/web/fastapi/demo17.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    @Author : tangkaize
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#    @Time : 2022/12/28 16:57
+#    @FIle： demo17.py
+#    @Software: PyCharm
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+
+if __name__ == '__main__':
+    uvicorn.run('demo17:app', host='127.0.0.1', reload=True)
