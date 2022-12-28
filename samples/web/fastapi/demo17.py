f4afae4f483c699c949eefecef0f878a85162aae
alex <li_703@qq.com>
2022-12-28 17:19:18 +0800

    第一章-fastapi-请求文件
    Signed-off-by: alex <li_703@qq.com>

--- samples/web/fastapi/demo17.py
+++ samples/web/fastapi/demo17.py
@@ -14,10 +14,12 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 
+from starlette.responses import HTMLResponse
+
 app = FastAPI()
 
 
-@app.post("/files/")
+@app.post("/file/")
 async def create_file(file: bytes = File()):
     return {"file_size": len(file)}
 
@@ -25,6 +27,34 @@ async def create_file(file: bytes = File()):
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}
+
+
+# 多文件上传
+@app.post('/files/')
+async def create_files(files: list[bytes] = File()):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post('/uploadfiles')
+async def create_upload_files(files: list[UploadFile] = File()):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
 
 if __name__ == '__main__':
