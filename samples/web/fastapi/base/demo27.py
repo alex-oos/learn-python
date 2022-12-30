1973f14bf568f2ee488b17ca688fd4380b5ef90b
alex <li_703@qq.com>
2022-12-30 21:24:46 +0800

    第一章-fastapi-base-全局依赖项
    Signed-off-by: alex <li_703@qq.com>

---
+++ samples/web/fastapi/base/demo27.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    @Author : tangkaize
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#    @Time : 2022/12/30 21:17
+#    @FIle： demo27.py
+#    @Software: PyCharm
+import uvicorn
+from fastapi import FastAPI, Header, HTTPException, Depends
+
+
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+app = FastAPI(dependencies=[Depends(verify_key), Depends(verify_token)])
+
+
+@app.get('/items/')
+async def read_items():
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+if __name__ == '__main__':
+    uvicorn.run('demo27:app', host='0.0.0.0', port=8000, reload=True)
