cdcea03f2cd96a8fa234f2d3e53fbc81fd0c8fec
alex <li_703@qq.com>
2022-12-30 21:16:50 +0800

    第一章-fastapi-base-路径操作装饰器依赖项
    Signed-off-by: alex <li_703@qq.com>

---
+++ samples/web/fastapi/base/demo26.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    @Author : tangkaize
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#    @Time : 2022/12/30 20:37
+#    @FIle： demo26.py
+#    @Software: PyCharm
+import uvicorn
+from fastapi import FastAPI, Depends, Header, HTTPException
+
+app = FastAPI()
+
+
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
+
+
+@app.get('/items/', dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
+
+
+if __name__ == '__main__':
+    uvicorn.run('demo26:app', host='0.0.0.0', port=8000, reload=True)
