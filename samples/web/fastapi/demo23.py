dacc2357567425d79c11650a2d198966ebb603ea
alex <li_703@qq.com>
2022-12-30 11:19:10 +0800

    第一章-fastapi-依赖项-第一步
    Signed-off-by: alex <li_703@qq.com>

---
+++ samples/web/fastapi/demo23.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    @Author : tangkaize
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#    @Time : 2022/12/30 11:09
+#    @FIle： demo23.py
+#    @Software: PyCharm
+from typing import Union
+
+import uvicorn
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+
+async def common_parameters(
+        q: Union[str, None] = None, skip: int = 0, limit: int = 100
+):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+if __name__ == '__main__':
+    uvicorn.run('demo23:app', host='0.0.0.0', port=8000)
