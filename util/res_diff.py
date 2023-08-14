021b57959e72090132879c3f66acb4db42000472
Alex <46887064+alex-oos@users.noreply.github.com>
2023-08-14 11:16:17 +0800

    add 获取文件替换文件

--- util/res_diff.py
+++ util/res_diff.py
@@ -8,37 +8,45 @@
 #    [________]_|__|________)<     
 #     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
 # +--+--+--+--+--+--+--+--+--+--+--+--+--+
-#    @Time : 2023/8/14 上午10:01
+#    @Time : 2023/8/14 上午11:00
 #    @FIle： res_diff.py
 #    @Software: PyCharm
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   Alex <alex@gmail.com
+#
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |]
+#    [________]_|__|________)<     |MENG|
+#     oo    oo  'oo OOOO-| oo\_   ~o~~~o~'
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#                        2019-09-20  13:46
+
+import re
 
 
-def res_diff(src_data, dst_data, path='', result=dict) -> dict:
-    """
-    比较两个字典的差异
-    :param src_data: 源数据
-    :param dst_data: 目标数据
-    :param path: 路径
-    :param result: 结果
-    :return: 返回差异数据
-    """
+def res_diff(src_data, dst_data, path='', diff=dict()):
+
     if isinstance(src_data, dict):
         for key in src_data:
-            new_path = path + '.' + key
-            if dst_data is not None and key in dst_data:
-                result = res_diff(src_data[key], dst_data[key], new_path, result)
+            newpath = path + '.' + key
+            if key in dst_data:
+                res_diff(src_data[key], dst_data[key], newpath, diff)
+            else:
+                diff[newpath] = [src_data.get(key), 'not exist is key']
     elif isinstance(src_data, list):
         if len(src_data) == len(dst_data):
             tmp = zip(src_data, dst_data)
             for k, v in enumerate(tmp):
-                result = res_diff(v[0], v[1], path + '.' + str(k), result)
-            else:
-                result[path] = [src_data, dst_data]
+                diff = res_diff(v[0], v[1], path + '.' + str(k), diff)
+        else:
+            diff[path] = [src_data, dst_data]
     else:
         if src_data != dst_data:
-            result[path] = [src_data, dst_data]
+            diff[path] = [src_data, dst_data]
 
-    return result
+    return diff
 
 
 if __name__ == "__main__":
